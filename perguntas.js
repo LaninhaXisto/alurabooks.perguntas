62d5ba1a6688882a5e730ea8eafe9afad7436e51
laninha <148967264+LaninhaXisto@users.noreply.github.com>
2024-09-25 14:34:07 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Definição de Flor',
+    'O que são flores?',
+    'Estruturas reprodutivas das plantas.'
+)
+
+criaCartao(
+    'Função das Flores',
+    'Qual é a função principal das flores?',
+    'Facilitar a reprodução através da polinização.'
+)
+
+criaCartao(
+    'Partes da Flor',
+    'Quais são as partes principais de uma flor?',
+    'Pétalas, sépalas, estames e pistilo.'
+)
+
+criaCartao(
+    'Processo de Polinização',
+    'O que é polinização?',
+    'Transferência de pólen de uma flor para outra.'
+)
+
+criaCartao(
+    'Flor Famosa',
+    'Qual é a flor mais famosa do mundo?',
+    'A rosa.'
+)
+
+criaCartao(
+    'Flores em Perfumes',
+    'Qual é a flor mais famosa do mundo?',
+    'Jasmim, seguido pela baunilha.'
+)
